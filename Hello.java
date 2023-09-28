3e69aac19418f3ae12412ee829f87d67decfe127
Klaurent Buci <klaurent@umich.edu>
2023-09-28 17:07:50 -0400

    comment changes

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        for(int i=1;i<=12;i++) {
+        // this is the task comment on classmate's folder
+        for(int i=1;i<=12;i++) { 
             System.out.println("Hello World..."+i);
         }
     }
